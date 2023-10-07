38fe0c0803985ce7614ff60a455b7b209dbde308
KStocky <kstocky1@gmail.com>
2023-10-07 16:29:03 +0100

    Get D3D SDK Version from nuget package

--- cmake/NugetPackages.cmake
+++ cmake/NugetPackages.cmake
@@ -2,16 +2,21 @@ include_guard(GLOBAL)
 
 include("${CMAKE_HOME_DIRECTORY}/cmake/Nuget.cmake")
 
-function(nuget_get_agility_sdk OUT_SUCCEEDED OUT_INCLUDE_PATH OUT_BINARY_PATH)
+function(nuget_get_agility_sdk OUT_SUCCEEDED OUT_INCLUDE_PATH OUT_BINARY_PATH OUT_SDK_VER)
 
-    nuget_pkg_get("Microsoft.Direct3D.D3D12" "1.610.5" ${OUT_SUCCEEDED} PKG_PATH)
+    set(MAJOR_VER "1")
+    set(MINOR_VER "610")
+    set(PATCH_VER "5")
+
+    nuget_pkg_get("Microsoft.Direct3D.D3D12" "${MAJOR_VER}.${MINOR_VER}.${PATCH_VER}" ${OUT_SUCCEEDED} PKG_PATH)
 
     if (${${OUT_SUCCEEDED}})
         set(${OUT_INCLUDE_PATH} "${PKG_PATH}/build/native/include")
         set(${OUT_BINARY_PATH} "${PKG_PATH}/build/native/bin/x64")
+        set(${OUT_SDK_VER} "${MINOR_VER}")
     endif()
 
-    return(PROPAGATE ${OUT_SUCCEEDED} ${OUT_INCLUDE_PATH} ${OUT_BINARY_PATH})
+    return(PROPAGATE ${OUT_SUCCEEDED} ${OUT_INCLUDE_PATH} ${OUT_BINARY_PATH} ${OUT_SDK_VER})
 endfunction()
 
 function(nuget_get_dxc OUT_SUCCEEDED OUT_INCLUDE_PATH OUT_BINARY_PATH OUT_LIB_PATH)
