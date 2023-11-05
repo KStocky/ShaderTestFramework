ebd9f5b97fe9c0d9813707b0d62d47148db494ff
KStocky <kstocky1@gmail.com>
2023-11-05 17:16:30 +0000

    Added more tests for the virtual directory mapping manager and fixing issues that the tests found

--- src/ShaderTestFramework/Private/D3D12/Shader/VirtualShaderDirectoryMappingManager.cpp
+++ src/ShaderTestFramework/Private/D3D12/Shader/VirtualShaderDirectoryMappingManager.cpp
@@ -18,6 +18,22 @@ namespace Private
 
 		return VirtualShaderDirectoryMappingManager::EErrorType::Success;
 	}
+
+	auto FindMapping(const std::vector<VirtualShaderDirectoryMapping>& InMappings, const std::filesystem::path& InVirtualPath)
+	{
+		return std::ranges::find_if(InMappings,
+			[&InVirtualPath](const VirtualShaderDirectoryMapping& In)
+			{
+				if (InVirtualPath.native().size() <= In.VirtualPath.native().size())
+				{
+					return false;
+				}
+				const auto startsWithMapping = InVirtualPath.native().starts_with(In.VirtualPath.native());
+				const auto charAfterVirtualMap = *(InVirtualPath.native().cbegin() + In.VirtualPath.native().size());
+				const auto virtualMappingEndsWithSlash = charAfterVirtualMap == L'/' || charAfterVirtualMap == L'\\';
+				return startsWithMapping && virtualMappingEndsWithSlash;
+			});
+	}
 }
 
 VirtualShaderDirectoryMappingManager::EErrorType VirtualShaderDirectoryMappingManager::AddMapping(VirtualShaderDirectoryMapping InMapping)
@@ -32,11 +48,7 @@ VirtualShaderDirectoryMappingManager::EErrorType VirtualShaderDirectoryMappingMa
 		return EErrorType::RealPathMustBeAbsolute;
 	}
 
-	if (auto foundMapping = std::ranges::find_if(m_Mappings,
-		[&InMapping](const VirtualShaderDirectoryMapping& In)
-		{
-			return In.VirtualPath.root_path() == InMapping.VirtualPath.root_path();
-		}); foundMapping != m_Mappings.cend())
+	if (auto foundMapping = Private::FindMapping(m_Mappings, InMapping.VirtualPath); foundMapping != m_Mappings.cend())
 	{
 		return EErrorType::VirtualPathAlreadyExists;
 	}
@@ -53,20 +65,12 @@ VirtualShaderDirectoryMappingManager::Expected<std::filesystem::path> VirtualSha
 		return Unexpected{ validateResult };
 	}
 
-	if (auto foundMapping = std::ranges::find_if(m_Mappings,
-		[&InVirtualPath](const VirtualShaderDirectoryMapping& In)
-		{
-			if (InVirtualPath.native().size() <= In.VirtualPath.native().size())
-			{
-				return false;
-			}
-			const auto startsWithMapping = InVirtualPath.native().starts_with(In.VirtualPath.native());
-			const auto virtualMappingEndsWithSlash = *(InVirtualPath.native().cbegin() + In.VirtualPath.native().size()) == L'/';
-			return startsWithMapping && virtualMappingEndsWithSlash;
-		}); foundMapping != m_Mappings.cend())
+	if (auto foundMapping = Private::FindMapping(m_Mappings, InVirtualPath); foundMapping != m_Mappings.cend())
 	{
 		std::wstring pathString = InVirtualPath;
-		pathString.replace_with_range(pathString.cbegin(), pathString.cbegin() + foundMapping->VirtualPath.native().size(), foundMapping->RealPath.native());
+		const auto& realPathString = foundMapping->RealPath.native();
+		const auto realPathView = std::wstring_view{ realPathString.c_str(), realPathString.ends_with(L'/') ? realPathString.size() - 1 : realPathString.size()};
+		pathString.replace_with_range(pathString.cbegin(), pathString.cbegin() + foundMapping->VirtualPath.native().size(), realPathView);
 
 		return std::filesystem::path{ std::move(pathString) };
 	}
--- test/D3D12/Shader/VirtualShaderDirectoryMappingManagerTests.cpp
+++ test/D3D12/Shader/VirtualShaderDirectoryMappingManagerTests.cpp
@@ -23,11 +23,40 @@ SCENARIO("VirtualShaderDirectoryMappingManagerTests")
 				}
 			}
 		}
+
+		WHEN("mapping with empty virtual path")
+		{
+			const auto result = manager.AddMapping({L"", L"C:/Hi"});
+
+			THEN("map addition failed")
+			{
+				REQUIRE(result == VirtualShaderDirectoryMappingManager::EErrorType::VirtualPathEmpty);
+			}
+		}
+
+		WHEN("mapping that does not start with slash added")
+		{
+			const auto result = manager.AddMapping({ L"Virtual", L"C:/Hi" });
+
+			THEN("map addition failed")
+			{
+				REQUIRE(result == VirtualShaderDirectoryMappingManager::EErrorType::VirtualPathShouldStartWithASlash);
+			}
+		}
+
+		WHEN("mapping has non absolute real path")
+		{
+			const auto result = manager.AddMapping({ L"/Virtual", L"../Hi" });
+
+			THEN("map addition failed")
+			{
+				REQUIRE(result == VirtualShaderDirectoryMappingManager::EErrorType::RealPathMustBeAbsolute);
+			}
+		}
 	}
 
-	GIVEN("Manger does have the requested mapping")
+	GIVEN("Manager does have a mapping")
 	{
-		const std::filesystem::path realStem = "/This/Exists.hlsl";
 		const std::filesystem::path realRoot = "C:/";
 
 		const std::filesystem::path virtualMapping = "/Virtual";
@@ -36,30 +65,31 @@ SCENARIO("VirtualShaderDirectoryMappingManagerTests")
 
 		REQUIRE(addResult == VirtualShaderDirectoryMappingManager::EErrorType::Success);
 
-		WHEN("file with virtual root requested")
+		WHEN("file with existing virtual mapping requested")
 		{
-			std::filesystem::path virtualPath = virtualMapping;
-			virtualPath += realStem;
+			std::filesystem::path virtualPath = "/Virtual/This/Exists.hlsl";
 			const auto result = manager.Map(virtualPath);
 
 			THEN("mapping succeeded")
 			{
+				const auto error = result.has_value() ? VirtualShaderDirectoryMappingManager::EErrorType::Success : result.error();
+				CAPTURE(error);
 				REQUIRE(result.has_value());
 
 				AND_THEN("result has expected path")
 				{
 					std::filesystem::path realPath = realRoot;
-					realPath += realStem;
+					realPath += "This/Exists.hlsl";
 					REQUIRE(result.value() == realPath);
 				}
 			}
 		}
 
-		WHEN("file with different virtual root requested")
+		WHEN("file with different virtual mapping requested")
 		{
 			std::filesystem::path virtualPath = virtualMapping;
 			virtualPath += "Foo/";
-			virtualPath += realStem;
+			virtualPath += "Failed.hlsl";
 			const auto result = manager.Map(virtualPath);
 
 			THEN("error returned")
@@ -70,6 +100,57 @@ SCENARIO("VirtualShaderDirectoryMappingManagerTests")
 				{
 					REQUIRE(result.error() == VirtualShaderDirectoryMappingManager::EErrorType::NoMappingFound);
 				}
+			}
+		}
+
+		WHEN("new mapping that starts the same is added")
+		{
+			std::filesystem::path newVirtualMapping = virtualMapping;
+			newVirtualMapping += "Foo";
+			const std::filesystem::path newRealRoot = "D:/Cool";
+
+			const auto mappingResult = manager.AddMapping({ newVirtualMapping, newRealRoot });
+
+			THEN("map addition succeeded")
+			{
+				CAPTURE(mappingResult);
+				REQUIRE(mappingResult == VirtualShaderDirectoryMappingManager::EErrorType::Success);
+
+				AND_WHEN("mapping requested with new virtual path")
+				{
+					std::filesystem::path virtualPath = newVirtualMapping;
+					virtualPath += "/Exists.hlsl";
+					const auto result = manager.Map(virtualPath);
+
+					THEN("mapping succeeds")
+					{
+						const auto error = result.has_value() ? VirtualShaderDirectoryMappingManager::EErrorType::Success : result.error();
+						CAPTURE(error);
+						REQUIRE(result.has_value());
+
+						AND_THEN("result has expected path")
+						{
+							std::filesystem::path realPath = newRealRoot;
+							realPath += "/Exists.hlsl";
+							REQUIRE(result.value() == realPath);
+						}
+					}
+				}
+			}
+		}
+
+		WHEN("new mapping that has same root as previous mapping")
+		{
+			std::filesystem::path newVirtualMapping = virtualMapping;
+			newVirtualMapping += "/Foo";
+			const std::filesystem::path newRealRoot = "D:/Cool";
+
+			const auto mappingResult = manager.AddMapping({ newVirtualMapping, newRealRoot });
+
+			THEN("map addition failed")
+			{
+				CAPTURE(mappingResult);
+				REQUIRE(mappingResult == VirtualShaderDirectoryMappingManager::EErrorType::VirtualPathAlreadyExists);
 			}
 		}
 	}
