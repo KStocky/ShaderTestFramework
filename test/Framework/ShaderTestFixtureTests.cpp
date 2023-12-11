04c930fd3077ab332b6a2bec08df1a17850b259c
KStocky <kstocky1@gmail.com>
2023-12-11 11:08:34 +0000

    Num args macro

--- test/Framework/ShaderTestFixtureTests.cpp
+++ test/Framework/ShaderTestFixtureTests.cpp
@@ -421,6 +421,22 @@ SCENARIO("HLSLFrameworkTests - Asserts - IsFalse")
     }
 }
 
+SCENARIO("HLSLFrameworkTests - Macros - NumArgs")
+{
+    auto testName = GENERATE
+    (
+        "GIVEN_ZeroArgs_WHEN_Counted_THEN_ReturnsZero",
+        "GIVEN_OneArg_WHEN_Counted_THEN_ReturnsOne",
+        "GIVEN_TenArgs_WHEN_Counted_THEN_ReturnsTen"
+    );
+
+    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/Macros/NumArgs.hlsl")));
+    DYNAMIC_SECTION(testName)
+    {
+        REQUIRE(fixture.RunTest(testName, 1, 1, 1));
+    }
+}
+
 SCENARIO("HLSLFrameworkTests - Macros - SectionVarCreation")
 {
     auto testName = GENERATE
---
+++ test/Shader/Framework/ShaderTestFixtureTests/HLSLFrameworkTests/Macros/NumArgs.hlsl
@@ -0,0 +1,22 @@
+#include "/Test/Public/ShaderTestFramework.hlsli"
+
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
+void GIVEN_ZeroArgs_WHEN_Counted_THEN_ReturnsZero()
+{
+    STF::AreEqual(0, STF_NUM_ARGS());
+}
+
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
+void GIVEN_OneArg_WHEN_Counted_THEN_ReturnsOne()
+{
+    STF::AreEqual(1, STF_NUM_ARGS(Hello));
+}
+
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
+void GIVEN_TenArgs_WHEN_Counted_THEN_ReturnsTen()
+{
+    STF::AreEqual(10, STF_NUM_ARGS(a, b, c, d, e, f, g, h, i, j));
+}
