a18395c512d1eeac81e7851065edf9484eb2e65e
KStocky <kstocky1@gmail.com>
2023-12-16 17:27:10 +0000

    Fixing github actions lack of VS 19.32.

--- src/ShaderTestFramework/Public/Utility/Concepts.h
+++ src/ShaderTestFramework/Public/Utility/Concepts.h
@@ -71,9 +71,52 @@ concept ConstexprDefaultConstructableEmptyCallableType = ConstexprDefaultConstru
 template<typename T>
 concept HLSLBaseType = std::same_as<T, i32> || std::same_as<T, u32> || std::same_as<T, float>;
 
+#if _MSC_VER < 1932
+
+namespace Private
+{
+    template <class Ty, class Context, class Formatter = Context::template formatter_type<std::remove_const_t<Ty>>>
+    concept Formattable_with = std::semiregular<Formatter>
+        && requires(Formatter & f, const Formatter & cf, Ty && t, Context fc,
+            std::basic_format_parse_context<typename Context::char_type> pc) {
+                { f.parse(pc) } -> std::same_as<typename decltype(pc)::iterator>;
+                { cf.format(t, fc) } -> std::same_as<typename Context::iterator>;
+    };
+    template <class CharT>
+    struct FakeFormatIter {
+
+        using iterator_category = std::output_iterator_tag;
+        using value_type = CharT;
+        using difference_type = ptrdiff_t;
+        using pointer = CharT*;
+        using reference = CharT&;
+
+        reference operator*() const;
+        pointer operator->() const;
+
+        FakeFormatIter& operator++();
+        FakeFormatIter operator++(int);
+    };
+
+    template <class Ty, class CharT>
+    concept formattable =
+        Formattable_with<std::remove_reference_t<Ty>, std::basic_format_context<FakeFormatIter<CharT>, CharT>>;
+
+}
+
+#endif // _HAS_CXX23
+
+template<typename T, typename CharT>
+concept Formattable =
+#if _MSC_VER >= 1932
+std::formattable<T, CharT>;
+#else
+Private::formattable<T, CharT>;
+#endif
+
 template<typename T>
 concept HLSLTypeTriviallyConvertibleType =
     std::is_trivially_copyable_v<T> &&
     (alignof(T) == 4) &&
-    std::formattable<T, char>;
+    Formattable<T, char>;
 
