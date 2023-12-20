0644f57d56d027aba057b97a410e6f722f0708f5
KStocky <kstocky1@gmail.com>
2023-12-20 17:16:14 +0000

    Added data read tests for the assert process function

--- test/Framework/AssertBufferProcessorTests.cpp
+++ test/Framework/AssertBufferProcessorTests.cpp
@@ -110,6 +110,123 @@ SCENARIO("AssertBufferProcessorTests - AssertInfo - No AssertData")
     }
 }
 
+SCENARIO("AssertBufferProcessorTests - AssertInfo - AssertData")
+{
+    auto serialize = []<typename T>(const std::span<T> InVals)
+    {
+        std::vector<std::byte> ret;
+        ret.resize(InVals.size_bytes() + InVals.size() * sizeof(u32));
+        const u64 sizeData = sizeof(T);
+        for (u64 writeIndex = 0; const auto& val : InVals)
+        {
+            std::memcpy(ret.data() + writeIndex, &sizeData, sizeof(u32));
+            writeIndex += sizeof(u32);
+
+            std::memcpy(ret.data() + writeIndex, &val, sizeData);
+            writeIndex += sizeData;
+        }
+
+        return ret;
+    };
+
+    auto [given, numFailed, expectedNumAsserts, layout, metaData, expectedAssertData] = GENERATE
+    (
+        table<std::string, u32, u64, STF::AssertBufferLayout, std::vector<STF::HLSLAssertMetaData>, std::vector<u32>>
+        (
+            {
+                std::tuple
+                { 
+                    "One failing assert with data", 
+                    1, 1, STF::AssertBufferLayout{1, 100}, 
+                    std::vector{ STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 1, 8 } },
+                    std::vector{4u}
+                },
+                std::tuple
+                {
+                    "One failing assert with no data",
+                    1, 1, STF::AssertBufferLayout{1, 100},
+                    std::vector{ STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 0, 0 } },
+                    std::vector<u32>{}
+                },
+                std::tuple
+                {
+                    "Two failing assert with no data",
+                    2, 2, STF::AssertBufferLayout{2, 100},
+                    std::vector{ STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 0, 0 }, STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 0, 0 } },
+                    std::vector<u32>{}
+                },
+                std::tuple
+                {
+                    "First fail has data, Second does not",
+                    2, 2, STF::AssertBufferLayout{2, 100},
+                    std::vector{ STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 2, 8 }, STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 0, 0 } },
+                    std::vector{16u}
+                },
+                std::tuple
+                {
+                    "First fail does not have data, Second does",
+                    2, 2, STF::AssertBufferLayout{2, 100},
+                    std::vector{ STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 0, 0 }, STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 2, 8 } },
+                    std::vector{16u}
+                },
+                std::tuple
+                {
+                    "Both asserts have data",
+                    2, 2, STF::AssertBufferLayout{2, 100},
+                    std::vector{ STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 2, 8 }, STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 2 + 8, 8 } },
+                    std::vector{16u, 32u}
+                }
+            }
+        )
+    );
+
+    std::vector<std::byte> buffer;
+    auto assertData = serialize(std::span{ expectedAssertData });
+    const u64 sizeMetaData = metaData.size() * sizeof(STF::HLSLAssertMetaData);
+    buffer.resize(sizeMetaData + assertData.size());
+
+    std::memcpy(buffer.data(), metaData.data(), sizeMetaData);
+    std::memcpy(buffer.data() + sizeMetaData, assertData.data(), assertData.size());
+
+    GIVEN(given)
+    {
+        WHEN("Processed")
+        {
+            const auto results = STF::ProcessAssertBuffer(0, numFailed, uint3(1, 1, 1), layout, std::as_bytes(std::span{ buffer }), STF::TypeConverterMap{});
+
+            THEN("Results has expected errors")
+            {
+                REQUIRE(results.NumSucceeded == 0);
+                REQUIRE(results.NumFailed == numFailed);
+                REQUIRE(results.FailedAsserts.size() == expectedNumAsserts);
+
+                if (expectedNumAsserts > 0)
+                {
+                    AND_THEN("assert data is as expected")
+                    {
+                        for (u64 assertIndex = 0; assertIndex < expectedNumAsserts; ++assertIndex)
+                        {
+                            const auto& metaInfo = metaData[assertIndex];
+                            if (metaInfo.DataSize > 0)
+                            {
+                                std::vector<std::byte> expectedData;
+                                expectedData.resize(8);
+                                std::memcpy(expectedData.data(), assertData.data() + (metaInfo.DataAddress - sizeMetaData), 8);
+
+                                REQUIRE(results.FailedAsserts[assertIndex].Data == expectedData);
+                            }
+                            else
+                            {
+                                REQUIRE(results.FailedAsserts[assertIndex].Data.empty());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 SCENARIO("AssertBufferProcessorTests - AssertInfo - Throwing")
 {
 
