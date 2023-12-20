188ddc47c1a104ee32456e968d0b9d548e35aecc
KStocky <kstocky1@gmail.com>
2023-12-20 17:49:10 +0000

    Type converter tests for the assert processor function

--- test/Framework/AssertBufferProcessorTests.cpp
+++ test/Framework/AssertBufferProcessorTests.cpp
@@ -7,6 +7,7 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
 
 SCENARIO("AssertBufferProcessorTests - Results")
 {
@@ -221,6 +222,139 @@ SCENARIO("AssertBufferProcessorTests - AssertInfo - AssertData")
                             }
                         }
                     }
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("AssertBufferProcessorTests - AssertInfo - Type Converter")
+{
+    using Catch::Matchers::ContainsSubstring;
+    auto serialize = []<typename T>(const std::span<T> InVals)
+    {
+        std::vector<std::byte> ret;
+        ret.resize(InVals.size_bytes() + InVals.size() * sizeof(u32));
+        const u64 sizeData = sizeof(T);
+        for (u64 writeIndex = 0; const auto & val : InVals)
+        {
+            std::memcpy(ret.data() + writeIndex, &sizeData, sizeof(u32));
+            writeIndex += sizeof(u32);
+
+            std::memcpy(ret.data() + writeIndex, &val, sizeData);
+            writeIndex += sizeData;
+        }
+
+        return ret;
+    };
+
+    auto conv1 =
+        [](const std::span<const std::byte> InBytes)
+        {
+            u32 val;
+            std::memcpy(&val, InBytes.data(), sizeof(u32));
+            return std::format("Type 1: {}", val);
+        };
+
+    auto conv2 =
+        [](const std::span<const std::byte> InBytes)
+        {
+            u32 val;
+            std::memcpy(&val, InBytes.data(), sizeof(u32));
+            return std::format("Type 2: {}", val);
+        };
+
+    auto [given, numFailed, layout, metaData, expectedAssertData, expectedSubstrings] = GENERATE
+    (
+        table<std::string, u32, STF::AssertBufferLayout, std::vector<STF::HLSLAssertMetaData>, std::vector<u32>, std::vector<std::string>>
+        (
+            {
+                std::tuple
+                {
+                    "One failing assert with no type converter",
+                    1, STF::AssertBufferLayout{1, 100},
+                    std::vector{ STF::HLSLAssertMetaData{ 42, 0, 0, 2, sizeof(STF::HLSLAssertMetaData) * 1, 8 } },
+                    std::vector{4u},
+                    std::vector<std::string>{"Bytes"}
+                },
+                std::tuple
+                {
+                    "One failing assert with type converter",
+                    1, STF::AssertBufferLayout{1, 100},
+                    std::vector{ STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 1, 8 } },
+                    std::vector{412u},
+                    std::vector<std::string>{"Type 1", "412"}
+                },
+                std::tuple
+                {
+                    "One failing assert with different type converter",
+                    1, STF::AssertBufferLayout{1, 100},
+                    std::vector{ STF::HLSLAssertMetaData{ 42, 0, 0, 1, sizeof(STF::HLSLAssertMetaData) * 1, 8 } },
+                    std::vector{412u},
+                    std::vector<std::string>{"Type 2", "412"}
+                },
+                std::tuple
+                {
+                    "Two failing asserts with same type converter",
+                    2, STF::AssertBufferLayout{2, 100},
+                    std::vector
+                    { 
+                        STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 2, 8 },
+                        STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 2 + 8, 8 }
+                    },
+                    std::vector{412u, 214u},
+                    std::vector<std::string>{"Type 1", "412", "214"}
+                },
+                std::tuple
+                {
+                    "Two failing asserts with different same type converter",
+                    2, STF::AssertBufferLayout{2, 100},
+                    std::vector
+                    {
+                        STF::HLSLAssertMetaData{ 42, 0, 0, 1, sizeof(STF::HLSLAssertMetaData) * 2, 8 },
+                        STF::HLSLAssertMetaData{ 42, 0, 0, 1, sizeof(STF::HLSLAssertMetaData) * 2 + 8, 8 }
+                    },
+                    std::vector{412u, 214u},
+                    std::vector<std::string>{"Type 2", "412", "214"}
+                },
+                std::tuple
+                {
+                    "Two failing asserts with different type converters",
+                    2, STF::AssertBufferLayout{2, 100},
+                    std::vector
+                    {
+                        STF::HLSLAssertMetaData{ 42, 0, 0, 0, sizeof(STF::HLSLAssertMetaData) * 2, 8 },
+                        STF::HLSLAssertMetaData{ 42, 0, 0, 1, sizeof(STF::HLSLAssertMetaData) * 2 + 8, 8 }
+                    },
+                    std::vector{412u, 214u},
+                    std::vector<std::string>{"Type 1", "Type 2", "412", "214"}
+                }
+            }
+        )
+    );
+
+    std::vector<std::byte> buffer;
+    auto assertData = serialize(std::span{ expectedAssertData });
+    const u64 sizeMetaData = metaData.size() * sizeof(STF::HLSLAssertMetaData);
+    buffer.resize(sizeMetaData + assertData.size());
+
+    std::memcpy(buffer.data(), metaData.data(), sizeMetaData);
+    std::memcpy(buffer.data() + sizeMetaData, assertData.data(), assertData.size());
+
+    GIVEN(given)
+    {
+        WHEN("Processed")
+        {
+            const auto results = STF::ProcessAssertBuffer(0, numFailed, uint3(1, 1, 1), layout, std::as_bytes(std::span{ buffer }), STF::TypeConverterMap{conv1, conv2});
+
+            THEN("Results has expected sub strings")
+            {
+                std::stringstream stream;
+                stream << results;
+
+                for (const auto expectedString : expectedSubstrings)
+                {
+                    REQUIRE_THAT(stream.str(), ContainsSubstring(expectedString, Catch::CaseSensitive::No));
                 }
             }
         }
