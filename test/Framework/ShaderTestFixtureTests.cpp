66bda9b2f3a199e0e0209fb9ca473f0abc44cd98
KStocky <kstocky1@gmail.com>
2023-12-20 12:14:54 +0000

    Additional tests for data writing for asserts

--- test/Framework/ShaderTestFixtureTests.cpp
+++ test/Framework/ShaderTestFixtureTests.cpp
@@ -280,17 +280,26 @@ SCENARIO("HLSLFrameworkTests - AssertBuffer - ResultProcessing - AssertInfoWithN
 SCENARIO("HLSLFrameworkTests - AssertBuffer - ResultProcessing - AssertInfoWithData")
 {
 
-    auto serialize = []<typename T>(const T& InVal)
+    auto serializeImpl = []<typename T>(const T& InVal, std::vector<std::byte>& InOutBytes)
+    {
+        static constexpr u32 size = sizeof(T);
+        const auto oldSize = InOutBytes.size();
+        InOutBytes.resize(InOutBytes.size() + sizeof(u32) + sizeof(T));
+        std::memcpy(InOutBytes.data() + oldSize, &size, sizeof(u32));
+        std::memcpy(InOutBytes.data() + oldSize + sizeof(u32), &InVal, size);
+    };
+
+    auto serialize = [&serializeImpl]<typename... T>(const T&... InVals)
     {
         std::vector<std::byte> ret;
-        static constexpr u32 size = sizeof(T);
-        ret.resize(sizeof(u32) + size);
-        std::memcpy(ret.data(), &size, sizeof(T));
-        std::memcpy(ret.data() + sizeof(u32), &InVal, size);
+        (serializeImpl(InVals, ret), ...);
         return ret;
     };
 
-    static constexpr u32 expectedValue = 34u;
+    auto test = serialize(32u, 1024u);
+
+    static constexpr u32 expectedValueLeft = 34u;
+    static constexpr u32 expectedValueRight = 12345678u;
 
     auto [testName, expected, numRecordedAsserts, numBytesData] = GENERATE_COPY
     (
@@ -306,7 +315,7 @@ SCENARIO("HLSLFrameworkTests - AssertBuffer - ResultProcessing - AssertInfoWithD
                 std::tuple
                 {
                     "GIVEN_AssertInfoAndDataCapacity_WHEN_FailedSingleAssertWithoutTypeIdWithWriter_THEN_HasExpectedResults",
-                    STF::TestRunResults{ {STF::FailedAssert{serialize(expectedValue), {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
+                    STF::TestRunResults{ {STF::FailedAssert{serialize(expectedValueLeft), {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
                     10, 400
                 },
                 std::tuple
@@ -318,8 +327,80 @@ SCENARIO("HLSLFrameworkTests - AssertBuffer - ResultProcessing - AssertInfoWithD
                 std::tuple
                 {
                     "GIVEN_AssertInfoAndDataCapacity_WHEN_FailedSingleAssertWithTypeIdWithWriter_THEN_HasExpectedResults",
-                    STF::TestRunResults{ {STF::FailedAssert{serialize(expectedValue), {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
+                    STF::TestRunResults{ {STF::FailedAssert{serialize(expectedValueLeft), {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
                     10, 400
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndDataCapacity_WHEN_FailedDoubleAssertWithoutTypeIdOrWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
+                    10, 400
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndDataCapacity_WHEN_FailedDoubleAssertWithoutTypeIdWithWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{serialize(expectedValueLeft, expectedValueRight), {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
+                    10, 400
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndDataCapacity_WHEN_FailedDoubleAssertWithTypeIdNoWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
+                    10, 400
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndDataCapacity_WHEN_FailedDoubleAssertWithTypeIdWithWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{serialize(expectedValueLeft, expectedValueRight), {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
+                    10, 400
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndNotEnoughDataCapacity_WHEN_FailedSingleAssertWithoutTypeIdOrWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
+                    10, 4
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndNotEnoughDataCapacity_WHEN_FailedSingleAssertWithoutTypeIdWithWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
+                    10, 4
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndNotEnoughDataCapacity_WHEN_FailedSingleAssertWithTypeIdNoWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
+                    10, 4
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndNotEnoughDataCapacity_WHEN_FailedSingleAssertWithTypeIdWithWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}}, 0, 1, uint3(1,1,1)},
+                    10, 4
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndDataCapacity_WHEN_LargeFailFirstThenSmallFailSingleAssertWithoutTypeIdWithWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}, STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}}, 0, 2, uint3(1,1,1)},
+                    10, 12
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndDataCapacity_WHEN_LargeFailFirstThenSmallFailSingleAssertWithTypeIdWithWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}, STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}}, 0, 2, uint3(1,1,1)},
+                    10, 12
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndDataCapacity_WHEN_SmallFailFirstThenLargeFailSingleAssertWithoutTypeIdWithWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{serialize(expectedValueRight), {}, STF::AssertMetaData{42, 0, 0}}, STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}}, 0, 2, uint3(1,1,1)},
+                    10, 12
+                },
+                std::tuple
+                {
+                    "GIVEN_AssertInfoAndDataCapacity_WHEN_SmallFailFirstThenLargeFailSingleAssertWithTypeIdWithWriter_THEN_HasExpectedResults",
+                    STF::TestRunResults{ {STF::FailedAssert{serialize(expectedValueRight), {}, STF::AssertMetaData{42, 0, 0}}, STF::FailedAssert{{}, {}, STF::AssertMetaData{42, 0, 0}}}, 0, 2, uint3(1,1,1)},
+                    10, 12
                 }
             }
         )
