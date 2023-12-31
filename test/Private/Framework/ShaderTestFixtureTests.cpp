08801de8fe3ab5c02cbc13e88ffd1614845be2ad
Keith Stockdale <kstocky1@gmail.com>
2023-12-31 15:53:10 +0000

    Moving NoAssertBuffer tests to their own cpp

--- test/Private/Framework/ShaderTestFixtureTests.cpp
+++ test/Private/Framework/ShaderTestFixtureTests.cpp
@@ -32,40 +32,6 @@ SCENARIO("ShaderTestFixtureTests")
     }
 }
 
-SCENARIO("HLSLFrameworkTests - AssertBuffer - ResultProcessing - NoAssertBuffer")
-{
-    auto [testName, numSucceeded, numFailed] = GENERATE
-    (
-        table<std::string, u32, u32>
-        (
-            {
-                std::tuple{ "GIVEN_ZeroAssertBuffer_WHEN_ZeroAssertsMade_THEN_HasExpectedResults", 0, 0},
-                std::tuple{ "GIVEN_ZeroAssertBuffer_WHEN_NonZeroSuccessfulAssertsMade_THEN_HasExpectedResults", 2, 0 },
-                std::tuple{ "GIVEN_ZeroAssertBuffer_WHEN_NonZeroFailedAssertsMade_THEN_HasExpectedResults", 0, 2 },
-                std::tuple{ "GIVEN_ZeroAssertBuffer_WHEN_NonZeroSuccessfulAndFailedAssertsMade_THEN_HasExpectedResults", 2, 2 },
-            }
-        )
-    );
-
-    const STF::TestRunResults expected
-    {
-        .FailedAsserts = {},
-        .NumSucceeded = numSucceeded,
-        .NumFailed = numFailed,
-        .DispatchDimensions = uint3(1,1,1)
-    };
-
-    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/AssertBuffer/ResultsProcessing/NoAssertBuffer.hlsl"), { 0, 0 }));
-    DYNAMIC_SECTION(testName)
-    {
-        const auto results = fixture.RunTest(testName, 1, 1, 1);
-        CAPTURE(results);
-        const auto actual = results.GetTestResults();
-        REQUIRE(actual);
-        REQUIRE(*actual == expected);
-    }
-}
-
 SCENARIO("HLSLFrameworkTests - AssertBuffer - ResultProcessing - AssertInfoWithNoData")
 {
     auto [testName, numRecordedAsserts, failedAsserts, numSucceeded, numFailed, dims] = GENERATE
