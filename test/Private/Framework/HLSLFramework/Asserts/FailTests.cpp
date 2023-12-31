1fa446db24b95537873f1bc69d403ec754e1d1e3
Keith Stockdale <kstocky1@gmail.com>
2023-12-31 17:50:18 +0000

    Fail tests moved to its own file

---
+++ test/Private/Framework/HLSLFramework/Asserts/FailTests.cpp
@@ -0,0 +1,32 @@
+#include "Framework/HLSLFramework/HLSLFrameworkTestsCommon.h"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+SCENARIO("HLSLFrameworkTests - Asserts - Fail")
+{
+    auto [testName, shouldSucceed] = GENERATE
+    (
+        table<std::string, bool>
+        (
+            {
+                std::tuple{"GIVEN_TestWithFailAssert_WHEN_Ran_THEN_Fails", false},
+                std::tuple{"GIVEN_Empty_WHEN_Ran_THEN_Succeeds", true}
+            }
+        )
+    );
+
+    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/Asserts/Fail.hlsl")));
+    DYNAMIC_SECTION(testName)
+    {
+        if (shouldSucceed)
+        {
+            REQUIRE(fixture.RunTest(testName, 1, 1, 1));
+        }
+        else
+        {
+            const auto result = fixture.RunTest(testName, 1, 1, 1);
+            REQUIRE_FALSE(result);
+        }
+    }
+}
--- test/Private/Framework/ShaderTestFixtureTests.cpp
+++ test/Private/Framework/ShaderTestFixtureTests.cpp
@@ -32,34 +32,6 @@ SCENARIO("ShaderTestFixtureTests")
     }
 }
 
-SCENARIO("HLSLFrameworkTests - Asserts - Fail")
-{
-    auto [testName, shouldSucceed] = GENERATE
-    (
-        table<std::string, bool>
-        (
-            {
-                std::tuple{"GIVEN_TestWithFailAssert_WHEN_Ran_THEN_Fails", false},
-                std::tuple{"GIVEN_Empty_WHEN_Ran_THEN_Succeeds", true}
-            }
-        )
-    );
-
-    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/Asserts/Fail.hlsl")));
-    DYNAMIC_SECTION(testName)
-    {
-        if (shouldSucceed)
-        {
-            REQUIRE(fixture.RunTest(testName, 1, 1, 1));
-        }
-        else
-        {
-            const auto result = fixture.RunTest(testName, 1, 1, 1);
-            REQUIRE_FALSE(result);
-        }
-    }
-}
-
 SCENARIO("HLSLFrameworkTests - Asserts - IsFalse")
 {
     auto [testName, shouldSucceed] = GENERATE
