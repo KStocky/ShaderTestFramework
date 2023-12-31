a4d50a071ccc78e8e210cb746e6aab8d80daf7e6
Keith Stockdale <kstocky1@gmail.com>
2023-12-31 17:43:33 +0000

    Is True tests moved to own file

--- test/Private/Framework/ShaderTestFixtureTests.cpp
+++ test/Private/Framework/ShaderTestFixtureTests.cpp
@@ -32,40 +32,6 @@ SCENARIO("ShaderTestFixtureTests")
     }
 }
 
-SCENARIO("HLSLFrameworkTests - Asserts - IsTrue")
-{
-    auto [testName, shouldSucceed] = GENERATE
-    (
-        table<std::string, bool>
-        (
-            {
-                std::tuple{"GIVEN_TrueLiteral_WHEN_IsTrueCalled_THEN_Succeeds", true},
-                std::tuple{"GIVEN_TrueNonLiteral_WHEN_IsTrueCalled_THEN_Succeeds", true},
-                std::tuple{"GIVEN_TrueExpression_WHEN_IsTrueCalled_THEN_Succeeds", true},
-                std::tuple{"GIVEN_ObjectWithBoolCast_WHEN_ObjectsIsTrue_THEN_Succeeds", true},
-                std::tuple{"GIVEN_FalseLiteral_WHEN_IsTrueCalled_THEN_Fails", false},
-                std::tuple{"GIVEN_FalseNonLiteral_WHEN_IsTrueCalled_THEN_Fails", false},
-                std::tuple{"GIVEN_FalseExpression_WHEN_IsTrueCalled_THEN_Fails", false},
-                std::tuple{"GIVEN_ObjectWithBoolCast_WHEN_ObjectsIsFalse_THEN_Fails", false}
-            }
-        )
-    );
-
-    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/Asserts/IsTrue.hlsl")));
-    DYNAMIC_SECTION(testName)
-    {
-        if (shouldSucceed)
-        {
-            REQUIRE(fixture.RunTest(testName, 1, 1, 1));
-        }
-        else
-        {
-            const auto result = fixture.RunTest(testName, 1, 1, 1);
-            REQUIRE_FALSE(result);
-        }
-    }
-}
-
 SCENARIO("HLSLFrameworkTests - Asserts - Fail")
 {
     auto [testName, shouldSucceed] = GENERATE
