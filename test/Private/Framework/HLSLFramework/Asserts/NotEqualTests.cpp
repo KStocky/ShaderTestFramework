101bc6a9001070520bcca6460292e208c14685e1
Keith Stockdale <kstocky1@gmail.com>
2023-12-31 17:40:03 +0000

    Not Equal tests moved to own file

---
+++ test/Private/Framework/HLSLFramework/Asserts/NotEqualTests.cpp
@@ -0,0 +1,40 @@
+#include "Framework/HLSLFramework/HLSLFrameworkTestsCommon.h"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+SCENARIO("HLSLFrameworkTests - Asserts - NotEqual")
+{
+    auto [testName, shouldSucceed] = GENERATE
+    (
+        table<std::string, bool>
+        (
+            {
+                std::tuple{"GIVEN_TwoEqualInts_WHEN_NotEqualCalled_THEN_Fails", false},
+                std::tuple{"GIVEN_TwoNotEqualInts_WHEN_NotEqualCalled_THEN_Succeeds", true},
+                std::tuple{"GIVEN_TwoEqualFloat4_WHEN_NotEqualCalled_THEN_Fails", false},
+                std::tuple{"GIVEN_TwoNotEqualFloat4_WHEN_NotEqualCalled_THEN_Succeeds", true},
+                std::tuple{"GIVEN_TwoEqualNamedInts_WHEN_NotEqualCalled_THEN_Fails", false},
+                std::tuple{"GIVEN_TwoNotEqualNamedInts_WHEN_NotEqualCalled_THEN_Succeeds", true},
+                std::tuple{"GIVEN_TwoEqualNamedFloat4_WHEN_NotEqualCalled_THEN_Fails", false},
+                std::tuple{"GIVEN_TwoNotEqualNamedFloat4_WHEN_NotEqualCalled_THEN_Succeeds", true},
+                std::tuple{"GIVEN_TwoObjectsWithNotEqualOperatorOverloads_WHEN_ObjectsAreEqual_THEN_Fails", false},
+                std::tuple{"GIVEN_TwoObjectsWithNotEqualOperatorOverloads_WHEN_ObjectsAreNotEqual_THEN_Succeeds", true}
+            }
+        )
+    );
+
+    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/Asserts/NotEqual.hlsl")));
+    DYNAMIC_SECTION(testName)
+    {
+        if (shouldSucceed)
+        {
+            REQUIRE(fixture.RunTest(testName, 1, 1, 1));
+        }
+        else
+        {
+            const auto result = fixture.RunTest(testName, 1, 1, 1);
+            REQUIRE_FALSE(result);
+        }
+    }
+}
--- test/Private/Framework/ShaderTestFixtureTests.cpp
+++ test/Private/Framework/ShaderTestFixtureTests.cpp
@@ -32,42 +32,6 @@ SCENARIO("ShaderTestFixtureTests")
     }
 }
 
-SCENARIO("HLSLFrameworkTests - Asserts - NotEqual")
-{
-    auto [testName, shouldSucceed] = GENERATE
-    (
-        table<std::string, bool>
-        (
-            {
-                std::tuple{"GIVEN_TwoEqualInts_WHEN_NotEqualCalled_THEN_Fails", false},
-                std::tuple{"GIVEN_TwoNotEqualInts_WHEN_NotEqualCalled_THEN_Succeeds", true},
-                std::tuple{"GIVEN_TwoEqualFloat4_WHEN_NotEqualCalled_THEN_Fails", false},
-                std::tuple{"GIVEN_TwoNotEqualFloat4_WHEN_NotEqualCalled_THEN_Succeeds", true},
-                std::tuple{"GIVEN_TwoEqualNamedInts_WHEN_NotEqualCalled_THEN_Fails", false},
-                std::tuple{"GIVEN_TwoNotEqualNamedInts_WHEN_NotEqualCalled_THEN_Succeeds", true},
-                std::tuple{"GIVEN_TwoEqualNamedFloat4_WHEN_NotEqualCalled_THEN_Fails", false},
-                std::tuple{"GIVEN_TwoNotEqualNamedFloat4_WHEN_NotEqualCalled_THEN_Succeeds", true},
-                std::tuple{"GIVEN_TwoObjectsWithNotEqualOperatorOverloads_WHEN_ObjectsAreEqual_THEN_Fails", false},
-                std::tuple{"GIVEN_TwoObjectsWithNotEqualOperatorOverloads_WHEN_ObjectsAreNotEqual_THEN_Succeeds", true}
-            }
-        )
-    );
-
-    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/Asserts/NotEqual.hlsl")));
-    DYNAMIC_SECTION(testName)
-    {
-        if (shouldSucceed)
-        {
-            REQUIRE(fixture.RunTest(testName, 1, 1, 1));
-        }
-        else
-        {
-            const auto result = fixture.RunTest(testName, 1, 1, 1);
-            REQUIRE_FALSE(result);
-        }
-    }
-}
-
 SCENARIO("HLSLFrameworkTests - Asserts - IsTrue")
 {
     auto [testName, shouldSucceed] = GENERATE
