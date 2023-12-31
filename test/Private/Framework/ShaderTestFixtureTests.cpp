9bda831b54ea14feb7f23be16efd16504b0651de
Keith Stockdale <kstocky1@gmail.com>
2023-12-31 18:43:31 +0000

    Fundamental type tests moved to their own file

--- test/Private/Framework/ShaderTestFixtureTests.cpp
+++ test/Private/Framework/ShaderTestFixtureTests.cpp
@@ -32,20 +32,6 @@ SCENARIO("ShaderTestFixtureTests")
     }
 }
 
-SCENARIO("HLSLFrameworkTests - TypeTraits - FundamentalType")
-{
-    auto testName = GENERATE
-    (
-        "AllShouldPass"
-    );
-
-    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/TypeTraits/FundamentalTypeTests.hlsl")));
-    DYNAMIC_SECTION(testName)
-    {
-        REQUIRE(fixture.RunTest(testName, 1, 1, 1));
-    }
-}
-
 SCENARIO("HLSLFrameworkTests - TypeTraits - TypeID")
 {
     auto testName = GENERATE
