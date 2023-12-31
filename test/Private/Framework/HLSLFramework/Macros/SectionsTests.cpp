7a292793c007fd7b3ad8ef4f97a532d1d4f0d539
Keith Stockdale <kstocky1@gmail.com>
2023-12-31 18:12:28 +0000

    Sections tests moved to their own file

---
+++ test/Private/Framework/HLSLFramework/Macros/SectionsTests.cpp
@@ -0,0 +1,29 @@
+#include "Framework/HLSLFramework/HLSLFrameworkTestsCommon.h"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+SCENARIO("HLSLFrameworkTests - Macros - SECTIONS")
+{
+    auto testName = GENERATE
+    (
+        "GIVEN_EmptySection_WHEN_RanUsingBeginEnd_THEN_NoAssertMade",
+        "GIVEN_EmptySection_WHEN_RanUsingSingle_THEN_NoAssertMade",
+        "GIVEN_SingleSection_WHEN_RanUsingBeginEnd_THEN_SectionsEnteredOnce",
+        "GIVEN_SingleSection_WHEN_RanUsingSingle_THEN_SectionsEnteredOnce",
+        "GIVEN_TwoSections_WHEN_RanUsingBeginEnd_THEN_EachSectionIsEnteredOnce",
+        "GIVEN_TwoSections_WHEN_RanUsingSingle_THEN_EachSectionIsEnteredOnce",
+        "GIVEN_TwoSubSectionsWithOneNestedSubsection_WHEN_RanUsingBeginEnd_THEN_EachSectionIsEnteredOnce",
+        "GIVEN_TwoSubSectionsWithOneNestedSubsection_WHEN_RanUsingSingle_THEN_EachSectionIsEnteredOnce",
+        "GIVEN_TwoSubSectionsWithTwoNestedSubsections_WHEN_RanUsingBeginEnd_THEN_ExpectedSubsectionEntryOccurs",
+        "GIVEN_TwoSubSectionsWithTwoNestedSubsections_WHEN_RanUsingSingle_THEN_ExpectedSubsectionEntryOccurs",
+        "GIVEN_ThreeLevelsDeepSections_WHEN_RanUsingBeginEnd_THEN_ExpectedSubsectionEntryOccurs",
+        "GIVEN_ThreeLevelsDeepSections_WHEN_RanUsingSingle_THEN_ExpectedSubsectionEntryOccurs"
+    );
+
+    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/Macros/Sections.hlsl")));
+    DYNAMIC_SECTION(testName)
+    {
+        REQUIRE(fixture.RunTest(testName, 1, 1, 1));
+    }
+}
