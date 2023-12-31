cc060557a47fad212729fbd769479ce3b3a18385
Keith Stockdale <kstocky1@gmail.com>
2023-12-31 18:07:51 +0000

    Scenario tests moved to their own file

---
+++ test/Private/Framework/HLSLFramework/Macros/ScenarioTests.cpp
@@ -0,0 +1,25 @@
+#include "Framework/HLSLFramework/HLSLFrameworkTestsCommon.h"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+SCENARIO("HLSLFrameworkTests - Macros - SCENARIO")
+{
+    auto testName = GENERATE
+    (
+        "GIVEN_EmptyScenario_WHEN_Ran_THEN_NoAssertMade",
+        "GIVEN_ScenarioWithNoSections_WHEN_Ran_THEN_FunctionOnlyEnteredOnce",
+        "GIVEN_SingleSection_WHEN_Ran_THEN_SectionsEnteredOnce",
+        "GIVEN_TwoSections_WHEN_Ran_THEN_EachSectionIsEnteredOnce",
+        "GIVEN_TwoSubSectionsWithOneNestedSubsection_WHEN_Ran_THEN_EachSectionIsEnteredOnce",
+        "GIVEN_ScenarioWithoutId_WHEN_Ran_THEN_IdIsNone",
+        "GIVEN_ScenarioWithDispatchThreadId_WHEN_Ran_THEN_IdIsInt3",
+        "GIVEN_ScenarioWithIntId_WHEN_Ran_THEN_IdIsInt"
+    );
+
+    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/Macros/Scenario.hlsl")));
+    DYNAMIC_SECTION(testName)
+    {
+        REQUIRE(fixture.RunTest(testName, 1, 1, 1));
+    }
+}
--- test/Private/Framework/ShaderTestFixtureTests.cpp
+++ test/Private/Framework/ShaderTestFixtureTests.cpp
@@ -32,27 +32,6 @@ SCENARIO("ShaderTestFixtureTests")
     }
 }
 
-SCENARIO("HLSLFrameworkTests - Macros - SCENARIO")
-{
-    auto testName = GENERATE
-    (
-        "GIVEN_EmptyScenario_WHEN_Ran_THEN_NoAssertMade",
-        "GIVEN_ScenarioWithNoSections_WHEN_Ran_THEN_FunctionOnlyEnteredOnce",
-        "GIVEN_SingleSection_WHEN_Ran_THEN_SectionsEnteredOnce",
-        "GIVEN_TwoSections_WHEN_Ran_THEN_EachSectionIsEnteredOnce",
-        "GIVEN_TwoSubSectionsWithOneNestedSubsection_WHEN_Ran_THEN_EachSectionIsEnteredOnce",
-        "GIVEN_ScenarioWithoutId_WHEN_Ran_THEN_IdIsNone",
-        "GIVEN_ScenarioWithDispatchThreadId_WHEN_Ran_THEN_IdIsInt3",
-        "GIVEN_ScenarioWithIntId_WHEN_Ran_THEN_IdIsInt"
-    );
-
-    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/Macros/Scenario.hlsl")));
-    DYNAMIC_SECTION(testName)
-    {
-        REQUIRE(fixture.RunTest(testName, 1, 1, 1));
-    }
-}
-
 SCENARIO("HLSLFrameworkTests - Macros - SECTIONS")
 {
     auto testName = GENERATE
