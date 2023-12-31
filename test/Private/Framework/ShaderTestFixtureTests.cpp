8f39522bb09c20f11840faee28058cb60599e525
Keith Stockdale <kstocky1@gmail.com>
2023-12-31 18:05:32 +0000

    Section creation macro tests moved to their own file

--- test/Private/Framework/ShaderTestFixtureTests.cpp
+++ test/Private/Framework/ShaderTestFixtureTests.cpp
@@ -32,23 +32,6 @@ SCENARIO("ShaderTestFixtureTests")
     }
 }
 
-SCENARIO("HLSLFrameworkTests - Macros - SectionVarCreation")
-{
-    auto testName = GENERATE
-    (
-        "GIVEN_SingleSectionVarCreated_WHEN_Queried_THEN_ValueIsZero",
-        "GIVEN_TwoSectionVarsCreated_WHEN_Queried_THEN_ValueAreAsExpected",
-        "GIVEN_TwoSectionVarsCreatedInALoop_WHEN_Queried_THEN_ValueAreAsExpected"
-    );
-
-    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/HLSLFrameworkTests/Macros/SectionVarCreation.hlsl")));
-    DYNAMIC_SECTION(testName)
-    {
-        REQUIRE(fixture.RunTest(testName, 1, 1, 1));
-    }
-}
-
-
 SCENARIO("HLSLFrameworkTests - Macros - SCENARIO")
 {
     auto testName = GENERATE
