62a9714dece8fdb39cbdfa9a6718469bba4c945b
Keith Stockdale <kstocky1@gmail.com>
2023-12-31 18:17:39 +0000

    HLSL proof of concept tests moved to their own files

---
+++ test/Private/Framework/HLSLFramework/ProofOfConceptTests.cpp
@@ -0,0 +1,23 @@
+#include "Framework/HLSLFramework/HLSLFrameworkTestsCommon.h"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+SCENARIO("HLSLFrameworkTests - ProofOfConcept")
+{
+    auto testName = GENERATE
+    (
+        "GIVEN_TwoCallsToCounter_WHEN_Compared_THEN_AreDifferent",
+        "GIVEN_StaticGlobalArray_WHEN_Inspected_THEN_AllZeroed",
+        "SectionTest",
+        "GIVEN_TwoDifferentSizedStructs_WHEN_sizeofCalledOn_Them_THEN_CorrectSizeReported",
+        "GIVEN_SomeTypesWithAndWithoutASpecializations_WHEN_ApplyFuncCalledOnThem_THEN_ExpectedResultsReturned",
+        "VariadicMacroOverloading"
+    );
+
+    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path(std::format("/Tests/HLSLFrameworkTests/ProofOfConcept/{}.hlsl", testName))));
+    DYNAMIC_SECTION(testName)
+    {
+        REQUIRE(fixture.RunTest(testName, 1, 1, 1));
+    }
+}
--- test/Private/Framework/ShaderTestFixtureTests.cpp
+++ test/Private/Framework/ShaderTestFixtureTests.cpp
@@ -32,25 +32,6 @@ SCENARIO("ShaderTestFixtureTests")
     }
 }
 
-SCENARIO("HLSLFrameworkTests - ProofOfConcept")
-{
-    auto testName = GENERATE
-    (
-        "GIVEN_TwoCallsToCounter_WHEN_Compared_THEN_AreDifferent",
-        "GIVEN_StaticGlobalArray_WHEN_Inspected_THEN_AllZeroed",
-        "SectionTest",
-        "GIVEN_TwoDifferentSizedStructs_WHEN_sizeofCalledOn_Them_THEN_CorrectSizeReported",
-        "GIVEN_SomeTypesWithAndWithoutASpecializations_WHEN_ApplyFuncCalledOnThem_THEN_ExpectedResultsReturned",
-        "VariadicMacroOverloading"
-    );
-
-    ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path(std::format("/Tests/HLSLFrameworkTests/ProofOfConcept/{}.hlsl", testName))));
-    DYNAMIC_SECTION(testName)
-    {
-        REQUIRE(fixture.RunTest(testName, 1, 1, 1));
-    }
-}
-
 SCENARIO("HLSLFrameworkTests - Container_wrapper")
 {
     auto [testName, shouldSucceed] = GENERATE
