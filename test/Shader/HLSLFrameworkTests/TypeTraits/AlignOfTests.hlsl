812d2d3288cfcc828c6f294e4b3c338615c17e27
KStocky <kstocky1@gmail.com>
2024-01-20 18:36:29 +0000

    Added alignment tests for vector and matrix types

--- test/Shader/HLSLFrameworkTests/TypeTraits/AlignOfTests.hlsl
+++ test/Shader/HLSLFrameworkTests/TypeTraits/AlignOfTests.hlsl
@@ -31,6 +31,12 @@ void AllHaveExpectedAlignment()
     ASSERT(AreEqual, 8u, ttl::align_of<int64_t>::value);
     ASSERT(AreEqual, 4u, ttl::align_of<bool>::value);
 
+    ASSERT(AreEqual, 2u, ttl::align_of<int16_t3>::value);
+    ASSERT(AreEqual, 8u, ttl::align_of<int64_t4>::value);
+
+    ASSERT(AreEqual, 2u, ttl::align_of<int16_t3x4>::value);
+    ASSERT(AreEqual, 8u, ttl::align_of<int64_t4x2>::value);
+
     ASSERT(AreEqual, (uint)sizeof(UnknownAlignmentLessThan8), ttl::align_of<UnknownAlignmentLessThan8>::value);
     ASSERT(AreEqual, 8u, ttl::align_of<UnknownAlignmentGreaterThan8>::value);
     ASSERT(AreEqual, 4u, ttl::align_of<ReportedAlignment>::value);
