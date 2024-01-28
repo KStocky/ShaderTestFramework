a45cd3a52327f80b6840068410d3d22287e81baa
KStocky <kstocky1@gmail.com>
2024-01-28 12:05:04 +0000

    Revert "Converting some type trait tests suites to using CompileTimeTests rather than a full test run"
    This reverts commit a3dd6bf0d350f1132acde6d9b2ff111e8e53b7ce.

--- test/Shader/HLSLFrameworkTests/TypeTraits/ArrayTraitsTests.hlsl
+++ test/Shader/HLSLFrameworkTests/TypeTraits/ArrayTraitsTests.hlsl
@@ -1,60 +1,64 @@
-#include "/Test/TTL/static_assert.hlsli"
-#include "/Test/TTL/type_traits.hlsli"
+#include "/Test/STF/ShaderTestFramework.hlsli"
 
-
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
 void GIVEN_BuiltInArray_WHEN_ArrayTraitsQueried_THEN_TraitsAsExpected()
 {
     static const uint size = 6;
     using type = int[size];
     using traits = ttl::array_traits<type>;
 
-    STATIC_ASSERT(traits::is_array);
-    STATIC_ASSERT(traits::size == size);
-    STATIC_ASSERT((ttl::is_same<traits::element_type, int>::value));
+    STF::IsTrue(traits::is_array);
+    STF::AreEqual(traits::size, size);
+    STF::IsTrue(ttl::is_same<traits::element_type, int>::value);
 }
 
-
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
 void GIVEN_NonArray_WHEN_ArrayTraitsQueried_THEN_TraitsAsExpected()
 {
     using type = int;
     using traits = ttl::array_traits<type>;
 
-    STATIC_ASSERT(!traits::is_array);
-    STATIC_ASSERT(traits::size == 0u);
-    STATIC_ASSERT((ttl::is_same<traits::element_type, void>::value));
+    STF::IsFalse(traits::is_array);
+    STF::AreEqual(traits::size, 0u);
+    STF::IsTrue(ttl::is_same<traits::element_type, void>::value);
 }
 
-
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
 void GIVEN_VectorType_WHEN_ArrayTraitsQueried_THEN_TraitsAsExpected()
 {
     using type = int2;
     using traits = ttl::array_traits<type>;
 
-    STATIC_ASSERT(!traits::is_array);
-    STATIC_ASSERT(traits::size == 0u);
-    STATIC_ASSERT((ttl::is_same<traits::element_type, void>::value));
+    STF::IsFalse(traits::is_array);
+    STF::AreEqual(traits::size, 0u);
+    STF::IsTrue(ttl::is_same<traits::element_type, void>::value);
 }
 
-
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
 void GIVEN_Buffer_WHEN_ArrayTraitsQueried_THEN_TraitsAsExpected()
 {
     using type = Buffer<int>;
     using traits = ttl::array_traits<type>;
 
-    STATIC_ASSERT(!traits::is_array);
-    STATIC_ASSERT(traits::size == 0u);
-    STATIC_ASSERT((ttl::is_same<traits::element_type, void>::value));
+    STF::IsFalse(traits::is_array);
+    STF::AreEqual(traits::size, 0u);
+    STF::IsTrue(ttl::is_same<traits::element_type, void>::value);
 }
 
-
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
 void GIVEN_RWBuffer_WHEN_ArrayTraitsQueried_THEN_TraitsAsExpected()
 {
     using type = RWBuffer<int>;
     using traits = ttl::array_traits<type>;
 
-    STATIC_ASSERT(!traits::is_array);
-    STATIC_ASSERT(traits::size == 0u);
-    STATIC_ASSERT((ttl::is_same<traits::element_type, void>::value));
+    STF::IsFalse(traits::is_array);
+    STF::AreEqual(traits::size, 0u);
+    STF::IsTrue(ttl::is_same<traits::element_type, void>::value);
 }
 
 struct MyStruct
@@ -63,52 +67,50 @@ struct MyStruct
     float b;
 };
 
-
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
 void GIVEN_StructuredBuffer_WHEN_ArrayTraitsQueried_THEN_TraitsAsExpected()
 {
     using type = StructuredBuffer<MyStruct>;
     using traits = ttl::array_traits<type>;
 
-    STATIC_ASSERT(!traits::is_array);
-    STATIC_ASSERT(traits::size == 0u);
-    STATIC_ASSERT((ttl::is_same<traits::element_type, void>::value));
+    STF::IsFalse(traits::is_array);
+    STF::AreEqual(traits::size, 0u);
+    STF::IsTrue(ttl::is_same<traits::element_type, void>::value);
 }
 
-
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
 void GIVEN_RWStructuredBuffer_WHEN_ArrayTraitsQueried_THEN_TraitsAsExpected()
 {
     using type = RWStructuredBuffer<MyStruct>;
     using traits = ttl::array_traits<type>;
 
-    STATIC_ASSERT(!traits::is_array);
-    STATIC_ASSERT(traits::size == 0u);
-    STATIC_ASSERT((ttl::is_same<traits::element_type, void>::value));
+    STF::IsFalse(traits::is_array);
+    STF::AreEqual(traits::size, 0u);
+    STF::IsTrue(ttl::is_same<traits::element_type, void>::value);
 }
 
-
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
 void GIVEN_ByteAddressBuffer_WHEN_ArrayTraitsQueried_THEN_TraitsAsExpected()
 {
     using type = ByteAddressBuffer;
     using traits = ttl::array_traits<type>;
 
-    STATIC_ASSERT(!traits::is_array);
-    STATIC_ASSERT(traits::size == 0u);
-    STATIC_ASSERT((ttl::is_same<traits::element_type, void>::value));
+    STF::IsFalse(traits::is_array);
+    STF::AreEqual(traits::size, 0u);
+    STF::IsTrue(ttl::is_same<traits::element_type, void>::value);
 }
 
-
+[RootSignature(SHADER_TEST_RS)]
+[numthreads(1,1,1)]
 void GIVEN_RWByteAddressBuffer_WHEN_ArrayTraitsQueried_THEN_TraitsAsExpected()
 {
     using type = RWByteAddressBuffer;
     using traits = ttl::array_traits<type>;
 
-    STATIC_ASSERT(!traits::is_array);
-    STATIC_ASSERT(traits::size == 0u);
-    STATIC_ASSERT((ttl::is_same<traits::element_type, void>::value));
-}
-
-[numthreads(1,1,1)]
-void AllCompile()
-{
-    
+    STF::IsFalse(traits::is_array);
+    STF::AreEqual(traits::size, 0u);
+    STF::IsTrue(ttl::is_same<traits::element_type, void>::value);
 }
