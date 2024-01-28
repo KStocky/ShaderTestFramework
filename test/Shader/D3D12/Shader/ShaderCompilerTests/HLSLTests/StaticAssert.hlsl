18379b55c33a4d7c06324e7346f02cca798bcfbe
KStocky <kstocky1@gmail.com>
2024-01-28 18:12:05 +0000

    Further experimentations with STATIC_ASSERT.

--- test/Shader/D3D12/Shader/ShaderCompilerTests/HLSLTests/StaticAssert.hlsl
+++ test/Shader/D3D12/Shader/ShaderCompilerTests/HLSLTests/StaticAssert.hlsl
@@ -17,13 +17,38 @@ struct StaticAssert<true>{};
 #define STATIC_ASSERT3(Expression, ...) struct { static const int Val = ((int1)42)[!Expression]; }
 
 
-// This provides a similar error message to STATIC_ASSERT2 but it works anywhere.
+// This provides a similar error message to STATIC_ASSERT2 but it works in the global namespace.
 #define CONCAT_IMPL(x, y) x##y
 #define CONCAT(x, y) CONCAT_IMPL(x, y)
-#define STATIC_ASSERT4(Expression, ...) static const int CONCAT(ASSERT_VAR_, __COUNTER__) = ((int1)42)[!Expression];
+#define STATIC_ASSERT4(Expression, ...) static const int CONCAT(ASSERT_VAR_, __COUNTER__) = ((int1)42)[!Expression]
+
+// Doesn't work in structs. Apparently this is fine because it is static const. Otherwise it would fail
+// But then we couldn't use it globally because it would be seen as a shader input which is not ok.
+#define STATIC_ASSERT5(Expression, ...) static const StaticAssert<!!Expression> CONCAT(ASSERT_VAR_, __COUNTER__)
+
+// This just doesn't work whatsoever
+#define STATIC_ASSERT6(Expression, ...) using CONCAT(ASSERT_TYPE_, __COUNTER__) = StaticAssert<!!Expression>
+
+// This works!
+#define STATIC_ASSERT7(Expression, ...) static const vector<int, !!(Expression)> CONCAT(ASSERT_TYPE_, __COUNTER__)
 
 STATIC_ASSERT4(true);
 STATIC_ASSERT4(true);
+
+STATIC_ASSERT5(true);
+STATIC_ASSERT6(false);
+
+STATIC_ASSERT7(true);
+//STATIC_ASSERT7(false);
+
+struct A
+{
+    STATIC_ASSERT5(false);
+    STATIC_ASSERT6(false);
+
+    STATIC_ASSERT7(true);
+    //STATIC_ASSERT7(false);
+};
 
 [numthreads(1,1,1)]
 void Main()
