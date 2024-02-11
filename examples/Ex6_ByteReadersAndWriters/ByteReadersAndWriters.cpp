c5fb6629e8405905b91aec768598a9f97e2e4c3b
KStocky <kstocky1@gmail.com>
2024-02-11 17:14:16 +0000

    First example for Byte reader documentation

---
+++ examples/Ex6_ByteReadersAndWriters/ByteReadersAndWriters.cpp
@@ -0,0 +1,25 @@
+#include <Framework/ShaderTestFixture.h>
+
+#include <catch2/catch_test_macros.hpp>
+
+SCENARIO("Example5Tests - Failing tests With FundamentalTypes")
+{
+    ShaderTestFixture::Desc desc{};
+
+    // We can add virtual shader directory mappings to our shader test environment
+    // Here we are saying that if a file path begins with "/Shader" then it is a virtual file directory
+    // and should be replaced with the path that evaluates from current_path()/SHADER_SRC
+    // std::filesystem::current_path() returns the current working directory
+    // We set both the current working directory and the SHADER_SRC macro in our cmake script.
+    desc.Mappings.emplace_back(VirtualShaderDirectoryMapping{"/Shader", std::filesystem::current_path() / SHADER_SRC});
+
+    // Instead of just giving a string that has HLSL code in it we provide a path to our HLSL source file
+    // Here we use our virtual shader directory mapping rather than the relative path
+    desc.Source = std::filesystem::path{ "/Shader/FailingFundamentalTypeTests.hlsl" };
+
+    ShaderTestFixture fixture(std::move(desc));
+    
+    // RunTest takes the entry function of the shader to run and also the dispatch config.
+    // In this case we are launching a single threadgroup
+    REQUIRE(fixture.RunTest("Tests", 1, 1, 1));
+}
