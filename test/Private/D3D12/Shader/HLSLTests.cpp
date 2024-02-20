acefa538f190047d39581604e6dd56391731abda
KStocky <kstocky1@gmail.com>
2024-02-20 07:55:27 +0000

    Adding some HLSL tests around printers to test how to disable string use in unoptimized builds

--- test/Private/D3D12/Shader/HLSLTests.cpp
+++ test/Private/D3D12/Shader/HLSLTests.cpp
@@ -10,201 +10,251 @@ using namespace ShaderCompilerTestsCommon;
 
 SCENARIO("HLSLTests")
 {
-    auto [name, successCondition] =
+    auto [name, successCondition, flags] =
         GENERATE
         (
-            table<std::string, bool(*)(const EHLSLVersion)>
+            table<std::string, bool(*)(const EHLSLVersion), std::vector<std::wstring>>
             (
                 { 
                     std::tuple
                     {
                         "FunctionStyleMacro",
-                        [](const EHLSLVersion) { return true; }
+                        [](const EHLSLVersion) { return true; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "VariadicMacro",
-                        [](const EHLSLVersion) { return true; }
+                        [](const EHLSLVersion) { return true; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "GlobalString",
-                        [](const EHLSLVersion) { return true; }
+                        [](const EHLSLVersion) { return true; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "LocalStringAssignedToAUintArray",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "LocalString",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                      std::tuple
                     {
                         "OperatorBoolWithAnEmptyStruct",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "OperatorIntWithAnEmptyStruct",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "OperatorBool",
-                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; }
+                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "OperatorInt",
-                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; }
+                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "CounterMacro",
-                        [](const EHLSLVersion) { return true; }
+                        [](const EHLSLVersion) { return true; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "LineMacro",
-                        [](const EHLSLVersion) { return true; }
+                        [](const EHLSLVersion) { return true; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "StaticStructMemberInTemplatedStruct",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "StaticVariableInTemplatedFunction",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "StaticConstStructMemberInTemplatedStruct",
-                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; }
+                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "StaticStructMemberInNonTemplatedStruct",
-                        [](const EHLSLVersion) { return true; }
+                        [](const EHLSLVersion) { return true; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "MacroGeneratedStructWithStaticDataMember",
-                        [](const EHLSLVersion) { return true; }
+                        [](const EHLSLVersion) { return true; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "ForwardDeclareFunction",
-                        [](const EHLSLVersion) { return true; }
+                        [](const EHLSLVersion) { return true; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "ForwardDeclareFunctionInOtherFunction",
-                        [](const EHLSLVersion) { return true; }
+                        [](const EHLSLVersion) { return true; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "ForwardDeclareFunctionInOtherFunctionAndDefineIt",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "DefineStructMemberFunctionLater",
-                        [](const EHLSLVersion) { return true; }
+                        [](const EHLSLVersion) { return true; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "DefineStructMemberFunctionInFunction",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "CleanupAttribute",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "SizeofOperator",
-                        [](const EHLSLVersion) { return true; }
+                        [](const EHLSLVersion) { return true; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "HasMemberCheck",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "ArrayTemplateSpecialization",
-                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; }
+                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "NestedStructTemplates",
-                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; }
+                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "TernaryInTypeTrait",
-                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; }
+                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "DeferSFINAE",
-                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; }
+                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "GloballyCoherentInStruct",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "GloballyCoherentTypeModifier",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "CallOperator",
-                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; }
+                        [](const EHLSLVersion InVer) { return InVer == EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "Alignof",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "DeducingPackingOrder",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "UserDefinedVector",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "SizeofEnumType",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "IsEnum",
-                        [](const EHLSLVersion) { return false; }
+                        [](const EHLSLVersion) { return false; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "ExpressionSFINAE",
-                        [](const EHLSLVersion InVer) { return InVer >= EHLSLVersion::v2021; }
+                        [](const EHLSLVersion InVer) { return InVer >= EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{}
                     },
                     std::tuple
                     {
                         "StaticAssert",
-                        [](const EHLSLVersion InVer) { return InVer >= EHLSLVersion::v2021; }
+                        [](const EHLSLVersion InVer) { return InVer >= EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{}
+                    },
+                    std::tuple
+                    {
+                        "PrinterWithOptimizations",
+                        [](const EHLSLVersion InVer) { return InVer >= EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{}
+                    },
+                    std::tuple
+                    {
+                        "PrinterWithoutOptimizations",
+                        [](const EHLSLVersion InVer) { return InVer >= EHLSLVersion::v2021; },
+                        std::vector<std::wstring>{L"-Od", L"-D", L"OPTIMIZATIONS_ENABLED=0"}
                     }
                 }
             )
@@ -223,7 +273,7 @@ SCENARIO("HLSLTests")
     {
         WHEN("Compiled with HLSL version " << Enum::UnscopedName(hlslVersion))
         {
-            const auto job = CreateCompilationJob(EShaderType::Compute, D3D_SHADER_MODEL_6_7, hlslVersion, {}, std::format("/Tests/HLSLTests/{}.hlsl", name));
+            const auto job = CreateCompilationJob(EShaderType::Compute, D3D_SHADER_MODEL_6_7, hlslVersion, std::move(flags), std::format("/Tests/HLSLTests/{}.hlsl", name));
             const auto errors = compiler.CompileShader(job);
             if (successCondition(hlslVersion))
             {
