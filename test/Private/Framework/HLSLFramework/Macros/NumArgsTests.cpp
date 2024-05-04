a4a68ffbe02c957a9d3665a36b0b701fc1eedf49
KStocky <kstocky1@gmail.com>
2024-05-04 16:07:02 +0100

    Move Num_Args and Join to TTL from STF. Also change NumArgs tests to a compile time test

--- test/Private/Framework/HLSLFramework/Macros/NumArgsTests.cpp
+++ test/Private/Framework/HLSLFramework/Macros/NumArgsTests.cpp
@@ -6,16 +6,6 @@
 
 SCENARIO("HLSLFrameworkTests - Macros - NumArgs")
 {
-    auto testName = GENERATE
-    (
-        "GIVEN_ZeroArgs_WHEN_Counted_THEN_ReturnsZero",
-        "GIVEN_OneArg_WHEN_Counted_THEN_ReturnsOne",
-        "GIVEN_TenArgs_WHEN_Counted_THEN_ReturnsTen"
-    );
-
     ShaderTestFixture fixture(CreateDescForHLSLFrameworkTest(fs::path("/Tests/Macros/NumArgs.hlsl")));
-    DYNAMIC_SECTION(testName)
-    {
-        REQUIRE(fixture.RunTest(testName, 1, 1, 1));
-    }
+    REQUIRE(fixture.RunCompileTimeTest());
 }
