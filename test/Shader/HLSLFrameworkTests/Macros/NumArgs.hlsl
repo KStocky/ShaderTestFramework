c9c6fe096c089e837642adcdae3c70dc135355a9
KStocky <kstocky1@gmail.com>
2024-05-04 16:07:32 +0100

    Submit change to NumArgs tests

--- test/Shader/HLSLFrameworkTests/Macros/NumArgs.hlsl
+++ test/Shader/HLSLFrameworkTests/Macros/NumArgs.hlsl
@@ -1,6 +1,6 @@
 #include "/Test/TTL/macro.hlsli"
 #include "/Test/TTL/static_assert.hlsli"
 
-STATIC_ASSERT(1 == TTL_NUM_ARGS(), "Expected NUM_ARGS to return 0");
+STATIC_ASSERT(0 == TTL_NUM_ARGS(), "Expected NUM_ARGS to return 0");
 STATIC_ASSERT(1 == TTL_NUM_ARGS(Hello), "Expected NUM_ARGS to return 1");
 STATIC_ASSERT(10 == TTL_NUM_ARGS(a, b, c, d, e, f, g, h, i, j), "Expected NUM_ARGS to return 10");
