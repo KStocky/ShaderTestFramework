d8ea5d234f968900fcd88778a0fa416b23a73434
KStocky <kstocky1@gmail.com>
2024-05-15 08:44:46 +0100

    Allow TTL_STAMP to take extra arguments

--- test/Shader/HLSLFrameworkTests/Macros/Stamp.hlsl
+++ test/Shader/HLSLFrameworkTests/Macros/Stamp.hlsl
@@ -6,7 +6,7 @@
 [numthreads(1,1,1)]
 void StampTests()
 {
-    SCENARIO("TTL_STAMP Tests")
+    SCENARIO("STAMP Tests")
     {
         SECTION("GIVEN: Stamper that does nothing, succeeds")
         {
@@ -58,6 +58,47 @@ void StampTests()
             SECTION("Stamp 256")
             {
                 TTL_STAMP(256, STAMP_ADD)
+
+                ASSERT(AreEqual, value, 256);
+            }
+
+            #undef ADD_TO_VALUE
+            #undef STAMP_ADD
+        }
+
+        SECTION("GIVEN: A stamper that adds 1 to a value passed as an Argument")
+        {
+            #define ADD_TO_VALUE(InN, InValue) InValue += 1;
+            #define STAMP_ADD(InStamper, InN, InValue) InStamper(1, ADD_TO_VALUE, InValue)
+            int value = 0;
+            
+            SECTION("Stamp 4")
+            {
+                TTL_STAMP(4, STAMP_ADD, value)
+
+                ASSERT(AreEqual, value, 4);
+            }
+
+            
+            SECTION("Stamp 16")
+            {
+                TTL_STAMP(16, STAMP_ADD, value)
+
+                ASSERT(AreEqual, value, 16);
+            }
+
+            
+            SECTION("Stamp 64")
+            {
+                TTL_STAMP(64, STAMP_ADD, value)
+
+                ASSERT(AreEqual, value, 64);
+            }
+
+            
+            SECTION("Stamp 256")
+            {
+                TTL_STAMP(256, STAMP_ADD, value)
 
                 ASSERT(AreEqual, value, 256);
             }
