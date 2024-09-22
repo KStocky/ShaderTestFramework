bfb95d12d526913e193662bfdd3a3c6a95be1c36
KStocky <kstocky1@gmail.com>
2024-09-22 18:03:44 +0100

    Removing tl-expected now that we require an MSVC version greater than what tl-expected was used for

--- src/Public/Utility/Expected.h
+++ src/Public/Utility/Expected.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#if _MSC_VER > 1936
 #include <expected>
 
 namespace stf
@@ -11,15 +10,3 @@ namespace stf
     template<typename E>
     using Unexpected = std::unexpected<E>;
 }
-#else
-#include <tl/expected.hpp>
-
-namespace stf
-{
-    template<typename T, typename E>
-    using Expected = tl::expected<T, E>;
-
-    template<typename E>
-    using Unexpected = tl::unexpected<E>;
-}
-#endif
