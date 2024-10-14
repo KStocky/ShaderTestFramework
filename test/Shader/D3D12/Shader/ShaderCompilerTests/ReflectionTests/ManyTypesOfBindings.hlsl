235de4b73fc6f73915a88a54f13606f48368196c
KStocky <kstocky1@gmail.com>
2024-10-14 08:18:42 +0100

    Fixing previous commit by actually committing the shader change

--- test/Shader/D3D12/Shader/ShaderCompilerTests/ReflectionTests/ManyTypesOfBindings.hlsl
+++ test/Shader/D3D12/Shader/ShaderCompilerTests/ReflectionTests/ManyTypesOfBindings.hlsl
@@ -16,6 +16,7 @@ struct MyCBufferStruct
 {
     int3 data1;
     int4 data2;
+    Texture2D<int> tex1D;
 };
 
 ConstantBuffer<MyCBufferStruct> MyConstantBuffer : register(b0);
@@ -36,5 +37,11 @@ cbuffer MyClassicBuffer : register(b6, space3)
 void Main(uint3 DispatchThreadId : SV_DispatchThreadID)
 {
     BuffArray[0][DispatchThreadId.x] = MyNamespace::NamespacedStructBinding.data1 + MyNamespace::NamespacedStructBinding.data2 * MyNamespace::NamespacedValueBinding;
-    BuffArray[1][DispatchThreadId.x] = MyConstantBuffer.data1.x * MyConstantBuffer.data2.w + ValueBinding + MyStructBuffer[DispatchThreadId.x].data1 + data1;
+    BuffArray[1][DispatchThreadId.x] = 
+        MyConstantBuffer.data1.x * 
+        MyConstantBuffer.data2.w + 
+        ValueBinding + 
+        MyStructBuffer[DispatchThreadId.x].data1 + 
+        data1 + 
+        MyConstantBuffer.tex1D.Load(int3(0, 0, 0)).r;
 }
