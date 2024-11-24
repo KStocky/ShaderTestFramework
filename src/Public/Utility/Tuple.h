1e8047f5bdf00f61b50d8cda617be10ee696f134
KStocky <kstocky1@gmail.com>
2024-11-24 15:55:42 +0000

    Solving the issue introduced by https://developercommunity.visualstudio.com/t/Unexpected-behaviour-with-msvc::no_uniqu/10798165

--- src/Public/Utility/Tuple.h
+++ src/Public/Utility/Tuple.h
@@ -1,14 +1,21 @@
 #pragma once
-
+#include <tuple>
 #include <tuplet/tuple.hpp>
 
-namespace
+namespace stf
 {
+    // Necessary due to https://developercommunity.visualstudio.com/t/Unexpected-behaviour-with-msvc::no_uniqu/10798165
+    // TODO: When this is fixed, we should remove it and just use tuplet::tuple.
     template<typename... InTypes>
-    using Tuple = tuplet::tuple<InTypes...>;
+    using Tuple =
+#if _MSC_VER >= 1942
+        std::tuple<InTypes...>;
+#else
+        tuplet::tuple<InTypes...>;
+#endif
 
-    template <size_t I, tuplet::indexable Tup>
+    template <size_t I, typename Tup>
     constexpr decltype(auto) Get(Tup&& tup) {
-        return tuplet::get<I>(std::forward<Tup>(tup));
+        return get<I>(std::forward<Tup>(tup));
     }
 }
