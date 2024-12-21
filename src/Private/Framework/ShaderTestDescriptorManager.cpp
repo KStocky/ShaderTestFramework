f587d07e6775b5045424cfa8ed9bf4e059492e41
KStocky <kstocky1@gmail.com>
2024-12-21 11:35:22 +0000

    Descriptor manager with some tests

---
+++ src/Private/Framework/ShaderTestDescriptorManager.cpp
@@ -0,0 +1,116 @@
+
+#include "Framework/ShaderTestDescriptorManager.h"
+
+#include "Utility/Tuple.h"
+
+namespace stf
+{
+    ShaderTestDescriptorManager::ShaderTestDescriptorManager(CreationParams InParams)
+        : m_Device(InParams.Device)
+        , m_GPUHeap(InParams.Device->CreateDescriptorHeap(
+            D3D12_DESCRIPTOR_HEAP_DESC
+            {
+                .Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+                .NumDescriptors = InParams.InitialSize,
+                .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
+                .NodeMask = 0
+            }))
+        , m_CPUHeap(InParams.Device->CreateDescriptorHeap(
+            D3D12_DESCRIPTOR_HEAP_DESC
+            {
+                .Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+                .NumDescriptors = InParams.InitialSize,
+                .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
+                .NodeMask = 0
+            }))
+        , m_Allocator({.NumDescriptors = InParams.InitialSize})
+        , m_CPUDescriptors(m_CPUHeap->GetHeapRange())
+        , m_GPUDescriptors(m_GPUHeap->GetHeapRange())
+    {
+    }
+
+    ShaderTestDescriptorManager::Expected<ShaderTestUAV> ShaderTestDescriptorManager::CreateUAV(SharedPtr<GPUResource> InResource, const D3D12_UNORDERED_ACCESS_VIEW_DESC& InDesc)
+    {
+        using ErrorType = BindlessFreeListAllocator::EErrorType;
+        return m_Allocator.Allocate()
+            .transform(
+                [this, resource = std::move(InResource), &InDesc](const BindlessFreeListAllocator::BindlessIndex InHandle) mutable
+                {
+
+                    m_Device->CreateUnorderedAccessView(*resource, InDesc, ThrowIfUnexpected(m_CPUDescriptors[InHandle.GetIndex()]));
+                    return ShaderTestUAV
+                    {
+                        .Resource = std::move(resource),
+                        .Handle = InHandle
+                    };
+                })
+            .transform_error(
+                [](const ErrorType InError)
+                {
+                    ThrowIfFalse(InError == ErrorType::EmptyError);
+
+                    return EErrorType::AllocatorFull;
+                });
+    }
+
+    ShaderTestDescriptorManager::Expected<void> ShaderTestDescriptorManager::ReleaseUAV(const ShaderTestUAV& InUAV)
+    {
+        using ErrorType = BindlessFreeListAllocator::EErrorType;
+        return
+            m_Allocator.Release(InUAV.Handle)
+            .transform_error(
+                [](const ErrorType InError)
+                {
+                    ThrowIfFalse(InError == ErrorType::IndexAlreadyReleased);
+                    return EErrorType::DescriptorAlreadyFree;
+                }
+            );
+    }
+
+    ShaderTestDescriptorManager::Expected<SharedPtr<DescriptorHeap>> ShaderTestDescriptorManager::Resize(const u32 InNewSize)
+    {
+        if (m_Allocator.GetCapacity() >= InNewSize)
+        {
+            return Unexpected(EErrorType::AttemptedShrink);
+        }
+
+        auto copyDescriptorsToNewHeap = 
+            [this, InNewSize](const DescriptorRange InSrc, const D3D12_DESCRIPTOR_HEAP_FLAGS InFlags)
+            {
+                auto newHeap = m_Device->CreateDescriptorHeap(
+                    D3D12_DESCRIPTOR_HEAP_DESC
+                    {
+                        .Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+                        .NumDescriptors = InNewSize,
+                        .Flags = InFlags,
+                        .NodeMask = 0
+                    });
+                const auto destRange = newHeap->GetHeapRange();
+                m_Device->CopyDescriptors(destRange, InSrc, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+                return Tuple{ destRange, std::move(newHeap) };
+            };
+
+        const auto oldGPUHeap = m_GPUHeap;
+
+        std::tie(m_CPUDescriptors, m_CPUHeap) = copyDescriptorsToNewHeap(m_CPUDescriptors, D3D12_DESCRIPTOR_HEAP_FLAG_NONE);
+        std::tie(m_GPUDescriptors, m_GPUHeap) = copyDescriptorsToNewHeap(m_CPUDescriptors, D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE);
+
+        return oldGPUHeap;
+    }
+
+    u32 ShaderTestDescriptorManager::GetSize() const
+    {
+        return m_Allocator.GetSize();
+    }
+
+    u32 ShaderTestDescriptorManager::GetCapacity() const
+    {
+        return m_Allocator.GetCapacity();
+    }
+
+    void ShaderTestDescriptorManager::SetDescriptorHeap(CommandList& InCommandList)
+    {
+        InCommandList.SetDescriptorHeaps(*m_GPUHeap);
+    }
+}
---
+++ test/Private/Framework/ShaderTestDescriptorManagerTests.cpp
@@ -0,0 +1,78 @@
+
+#include <D3D12/GPUDevice.h>
+#include <Framework/ShaderTestDescriptorManager.h>
+#include <Utility/EnumReflection.h>
+#include <Utility/Pointer.h>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+namespace DescriptorManagerTestPrivate
+{
+    class Fixture
+    {
+    public:
+
+        Fixture()
+            : device(stf::MakeShared<stf::GPUDevice>(
+                stf::GPUDevice::CreationParams
+                {
+                }))
+            , resource(device->CreateCommittedResource(
+                CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+                D3D12_HEAP_FLAG_NONE,
+                CD3DX12_RESOURCE_DESC1::Buffer(1024, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS),
+                D3D12_BARRIER_LAYOUT_UNDEFINED))
+            , uavDesc(
+                D3D12_UNORDERED_ACCESS_VIEW_DESC
+                {
+                    .Format = DXGI_FORMAT_R32_TYPELESS,
+                    .ViewDimension = D3D12_UAV_DIMENSION_BUFFER,
+                    .Buffer
+                    {
+                        .FirstElement = 0,
+                        .NumElements = 15,
+                        .StructureByteStride = 0,
+                        .CounterOffsetInBytes = 0,
+                        .Flags = D3D12_BUFFER_UAV_FLAG_RAW
+                    }
+                })
+        {
+        }
+
+    protected:
+
+        stf::SharedPtr<stf::GPUDevice> device;
+        stf::SharedPtr<stf::GPUResource> resource;
+        D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc;
+    };
+}
+
+TEST_CASE_PERSISTENT_FIXTURE(DescriptorManagerTestPrivate::Fixture, "Descriptor Manager Tests")
+{
+    using namespace stf;
+    GIVEN("An initial size of 1")
+    {
+        auto manager = MakeShared<ShaderTestDescriptorManager>(
+            ShaderTestDescriptorManager::CreationParams{
+                .Device = device,
+                .InitialSize = 1
+            });
+
+        THEN("State is as expected")
+        {
+            REQUIRE(1 == manager->GetCapacity());
+            REQUIRE(0 == manager->GetSize());
+        }
+
+        WHEN("Allocation made")
+        {
+            auto firstAllocationResult = manager->CreateUAV(resource, uavDesc);
+
+            THEN("Succeeds")
+            {
+                REQUIRE(firstAllocationResult.has_value());
+            }
+        }
+    }
+}
