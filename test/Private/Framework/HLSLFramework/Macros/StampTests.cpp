29cc5e9433742a2540906e83acd10a2845b71e49
KStocky <kstocky1@gmail.com>
2025-11-30 19:31:41 +0000

    Fixing unity build error

--- test/Private/Framework/HLSLFramework/Macros/StampTests.cpp
+++ test/Private/Framework/HLSLFramework/Macros/StampTests.cpp
@@ -1,6 +1,7 @@
 #include "Framework/HLSLFramework/HLSLFrameworkTestsCommon.h"
 #include <Framework/ShaderTestFixture.h>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 #include <ranges>
 
