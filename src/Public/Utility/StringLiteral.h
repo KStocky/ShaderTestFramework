52bb9635e96cf75f6399c3aa7e81232a9db4de1e
KStocky <kstocky1@gmail.com>
2025-12-23 16:00:30 +0000

    String literal class for guaranteeing a string literal

---
+++ src/Public/Utility/StringLiteral.h
@@ -0,0 +1,30 @@
+
+#pragma once
+
+#include <string_view>
+
+namespace stf
+{
+    class StringLiteral
+    {
+    public:
+
+        consteval StringLiteral(const std::string_view InView)
+            : m_View(InView)
+        {
+        }
+
+        constexpr operator std::string_view() const
+        {
+            return View();
+        }
+
+        constexpr std::string_view View() const
+        {
+            return m_View;
+        }
+
+    private:
+        std::string_view m_View{};
+    };
+}
