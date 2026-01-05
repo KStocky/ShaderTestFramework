894d7bc226bfa4fb574cfd178a235c9559be5950
KStocky <kstocky1@gmail.com>
2026-01-05 09:44:41 +0000

    Fixing value binding test so that it is actually testing the thing it claims to test

--- test/Shader/HLSLFrameworkTests/Binding/ValueBindingsTests/GlobalBindingsTooLarge.hlsl
+++ test/Shader/HLSLFrameworkTests/Binding/ValueBindingsTests/GlobalBindingsTooLarge.hlsl
@@ -7,21 +7,23 @@ struct MyStruct
     float4 C;
 };
 
-MyStruct MyParam;
+MyStruct Param1;
 
-float2 D;
-int3 E;
+struct TooLarge
+{
+    float2 D;
+    int3 E;
 
-int4x4 F;
-int4x4 G;
-int4x4 H;
+    int4x4 F;
+    int4x4 G;
+    int4x4 H;
+};
+
+ConstantBuffer<TooLarge> Param2;
 
 [numthreads(1, 1, 1)]
 void Main()
 {
-    ASSERT(AreEqual, MyParam.A, 4.0f);
-    ASSERT(AreEqual, MyParam.B, 42);
-    ASSERT(AreEqual, MyParam.C, float4(1.0, 2.0, 3.0, 4.0));
-    ASSERT(AreEqual, D, float2(5.0, 6.0));
-    ASSERT(AreEqual, E, int3(123, 456, 789));
+    ASSERT(AreEqual, Param1.A, 4.0f);
+    ASSERT(AreEqual, Param2.E.x, 42);
 }
