99f6cc33523e816ac7a9a5f6cbebe6d254a8807b
KStocky <kstocky1@gmail.com>
2026-01-17 14:07:50 +0000

    Just make tuple use std::tuple for Clang 19 since clang 19 just can't handle tuples implemented as an aggregate

--- src/Public/Utility/Tuple.h
+++ src/Public/Utility/Tuple.h
@@ -1,13 +1,14 @@
 #pragma once
 #include <tuplet/tuple.hpp>
+#include <tuple>
 
 namespace stf
 {
+#if defined(__clang__) && __clang_major__ <= 19
+    template<typename... InTypes>
+    using Tuple = std::tuple<InTypes...>;
+#else
     template<typename... InTypes>
     using Tuple = tuplet::tuple<InTypes...>;
-
-    template <class... T>
-    constexpr Tuple<T&...> tie(T&... t) {
-        return { t... };
-    }
+#endif
 }
