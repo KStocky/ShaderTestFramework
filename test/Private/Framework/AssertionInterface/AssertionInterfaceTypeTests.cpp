68b91489eae3e81237f0f3a334523b31351af014
KStocky <kstocky1@gmail.com>
2026-04-06 18:14:53 +0100

    Added requirement for resource creation function to assertion interface.

--- test/Private/Framework/AssertionInterface/AssertionInterfaceTypeTests.cpp
+++ test/Private/Framework/AssertionInterface/AssertionInterfaceTypeTests.cpp
@@ -1,9 +1,13 @@
 
 #include "Framework/AssertionInterface.h"
 
+#include "D3D12/CommandEngine.h"
+#include "TestUtilities/UniqueType.h"
+
 namespace stf::assert::AssertionInterfaceTypeTests
 {
     struct Empty {};
+
     struct ValidTestRunResultsType
     {
         bool Succeeded() const { return true; }
@@ -19,13 +23,74 @@ namespace stf::assert::AssertionInterfaceTypeTests
     struct ValidType
     {
         using TestRunResultsType = ValidTestRunResultsType;
+        using GPUResourcesType = UniqueType<Empty>;
+
+        GPUResourcesType CreateGPUResources(ScopedCommandContext&)
+        {
+            return GPUResourcesType{};
+        }
     };
 
-    struct ResultsTypeInValid
+    struct InvalidResultsType
     {
-        using TestRunResultsType = Empty;
+        using TestRunResultsType = UniqueType<Empty>;
+
+        using GPUResourcesType = UniqueType<Empty>;
+
+        GPUResourcesType CreateGPUResources(ScopedCommandContext&)
+        {
+            return GPUResourcesType{};
+        }
+    };
+
+    struct CreateGPUResourcesReturnsWrongType
+    {
+        using TestRunResultsType = ValidTestRunResultsType;
+        using GPUResourcesType = UniqueType<Empty>;
+
+        auto CreateGPUResources(ScopedCommandContext&)
+        {
+            return UniqueType<Empty>{};
+        }
+    };
+
+    struct CreateGPUResourcesAcceptsByConstRef
+    {
+        using TestRunResultsType = ValidTestRunResultsType;
+        using GPUResourcesType = UniqueType<Empty>;
+
+        GPUResourcesType CreateGPUResources(const ScopedCommandContext&)
+        {
+            return GPUResourcesType{};
+        }
+    };
+
+    struct CreateGPUResourcesAcceptsByValue
+    {
+        using TestRunResultsType = ValidTestRunResultsType;
+        using GPUResourcesType = UniqueType<Empty>;
+
+        GPUResourcesType CreateGPUResources(ScopedCommandContext)
+        {
+            return GPUResourcesType{};
+        }
+    };
+
+    struct CreateGPUResourcesAcceptsByRValueRef
+    {
+        using TestRunResultsType = ValidTestRunResultsType;
+        using GPUResourcesType = UniqueType<Empty>;
+
+        GPUResourcesType CreateGPUResources(ScopedCommandContext&&)
+        {
+            return GPUResourcesType{};
+        }
     };
 
     static_assert(CAssertionInterfaceType<ValidType>, "Expected this type to be valid for the concept");
-    static_assert(!CAssertionInterfaceType<ResultsTypeInValid>, "Expected this type to not be valid for the concept");
+    static_assert(!CAssertionInterfaceType<InvalidResultsType>, "Expected this type to not be valid for the concept");
+    static_assert(!CAssertionInterfaceType<CreateGPUResourcesReturnsWrongType>, "Expected this type to not be valid for the concept");
+    static_assert(CAssertionInterfaceType<CreateGPUResourcesAcceptsByConstRef>, "Expected this type to be valid for the concept");
+    static_assert(!CAssertionInterfaceType<CreateGPUResourcesAcceptsByValue>, "Expected this type to not be valid for the concept");
+    static_assert(!CAssertionInterfaceType<CreateGPUResourcesAcceptsByRValueRef>, "Expected this type to not be valid for the concept");
 }
